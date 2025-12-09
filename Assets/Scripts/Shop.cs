dba68b47383474dba756d5ae5a856b761a38b9d9
brian feng <brainfeng135@gmail.com>
2025-12-09 01:19:21 -0800

    gnite

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-    public List<GameObject> ShopSlots;
+    [SerializeField] private List<ShopSlot> ShopSlots;
+    [SerializeField] private Button RerollButton;
+    [SerializeField] private Button BuyXPButton;
 }
